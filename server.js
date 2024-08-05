887e7f3fad80b472bf2927e7bd771d1f2c78a82a
Naitik Shah <120545959+Naitik1112@users.noreply.github.com>
2024-08-05 13:08:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const DB = process.env.DATABASE.replace(
 
 mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
-const port = process.env.PORT || 3100;
+const port = process.env.PORT ;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
